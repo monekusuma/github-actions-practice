e0f50885e55eef7722de317a13043a3e50af1ad7
Mone Kusuma <monekusuma3121@gmail.com>
2026-08-19 13:41:07 +0530

    Initialize Terraform configuration with local provider

---
+++ main.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_version = ">= 1.5.0"
+
+  required_providers {
+    local = {
+      source  = "hashicorp/local"
+      version = "~> 2.5"
+    }
+  }
+}
+
+provider "local" {}
+
+resource "local_file" "example" {
+  filename = "hello.txt"
+  content  = "Hello from Terraform and GitHub Actions!"
+}
